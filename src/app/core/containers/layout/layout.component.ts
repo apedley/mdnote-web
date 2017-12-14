727f87d092972002f5c8d7c77c750fc4d2056d30
Andrew Pedley <apedley@gmail.com>
2017-12-14 08:07:43 -0600

    no add note link for unauthenticated

--- src/app/core/containers/layout/layout.component.ts
+++ src/app/core/containers/layout/layout.component.ts
@@ -2,7 +2,6 @@ import { LayoutService } from '../../layout.service';
 import { AuthService } from '../../../auth/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { Category } from '../../../notes/models/category.model';
-import { NotesService } from '../../../notes/notes.service';
 import { ChangeDetectorRef, Component, OnDestroy, OnInit, Input } from '@angular/core';
 import {MediaMatcher, BreakpointObserver} from '@angular/cdk/layout';
 import { Observable } from 'rxjs/Observable';
@@ -18,6 +17,7 @@ export class LayoutComponent implements OnDestroy, OnInit {
   public sidebarOpen = true;
 
   public sidebar: Observable<boolean>;
+  public authenticated: Observable<boolean>;
 
   constructor(public changeDetectorRef: ChangeDetectorRef, public media: MediaMatcher, private route: ActivatedRoute, private authService: AuthService, private layoutService: LayoutService) {
     this.sidebarEnabled = !route.snapshot.data['hideSidebar'];
@@ -26,6 +26,7 @@ export class LayoutComponent implements OnDestroy, OnInit {
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
     this.sidebar = this.layoutService.getSidebarOpen();
+    this.authenticated = this.authService.getAuthenticated();
   }
 
   ngOnInit(): void {
